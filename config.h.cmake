53856704d8ac36932adbd930a2bfa38f956dafcb
Canadian Bacon <zach.thibeau@gmail.com>
2014-05-01 03:21:17 -0400

    Perform check's of certain files to build configuration file so that PChat can be built properly.

---
+++ config.h.cmake
@@ -0,0 +1,14 @@
+#cmakedefine ENABLE_NLS 1
+#cmakedefine USE_IPV6 1
+#cmakedefine USE_GMODULE 1
+#cmakedefine USE_PLUGIN 1
+#cmakedefine USE_OPENSSL 1
+#cmakedefine USE_IPV6 1
+#cmakedefine USE_IPV6 1
+#cmakedefine HAVE_ISO_CODES 1
+#cmakedefine PACKAGE_NAME
+#cmakedefine PACKAGE_VERSION
+#cmakedefine XCHATLIBDIR
+#cmakedefine XCHATSHAREDIR
+#cmakedefine GETTEXT_PACKAGE
+#cmakedefine PACKAGE_TARNAME
